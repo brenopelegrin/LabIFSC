8087805bd20508436126440d3880ad3f9fb58b65
viniciusdutra314 <dutravinisousa2@usp.br>
2023-09-13 21:49:42 -0300

    quantidade de numeros aleatorios customizavel
    variavel global chamada num_gaussianos que pode ser modificada com a função quantidade_numeros_gaussianos(valor), por padrão num_gaussianos=10_000

--- LabIFSC/__init__.py
+++ LabIFSC/__init__.py
@@ -8,11 +8,19 @@ __version__ = "0.1.13"
 __email__ = "gabrieljvnq@gmail.com"
 __status__ = "Production"
 
+num_gaussianos=int(1e5)    
+def quantidade_numeros_gaussianos(valor):
+    try: valor=int(valor)
+    except:
+        raise ValueError("Esse objeto não pode ser convertido em inteiro")
+    global num_gaussianos
+    num_gaussianos=valor
+
 from .geral import TODAS_AS_UNIDADES, MAPA_DE_DIMENSOES, PREFIXOS_SI_LONGOS, PREFIXOS_SI_CURTOS, PREFIXOS_SI, analisa_numero, analisa_unidades, calcula_dimensao, parse_dimensions, acha_unidade, unidades_em_texto
-from .medida import Medida, M, arrayM, MCarlo, montecarlo, LabIFSC_Mcarlo_samples
+from .medida import Medida, M, arrayM, MCarlo, montecarlo
 from .unidade import Unidade
 from .lista_de_unidades import registra_unidades ; registra_unidades()
-from .matematica import soma, cos, sin, tan, arc_cos, arc_sin, arc_tan, log, log10, log2, ln, sqrt, cbrt, dam, mean
+from .matematica import *; from .matematica import funcoes_matematicas
 from .tabela import media, desvio_padrao, linearize, compare, Tabela
 from .constantes import *
 
@@ -21,6 +29,5 @@ __all__ = [
     "TODAS_AS_UNIDADES", "MAPA_DE_DIMENSOES", "PREFIXOS_SI_LONGOS", "PREFIXOS_SI_CURTOS", "PREFIXOS_SI", "analisa_numero", "analisa_unidades", "calcula_dimensao", "parse_dimensions", "acha_unidade", "unidades_em_texto",
     "Medida", "M","MCarlo", "arrayM", "Tabela",
     "Unidade",
-    "registra_unidades",
-    "soma", "cos", "sin", "tan", "arc_cos", "arc_sin", "arc_tan", "log", "log10", "log2", "ln", "sqrt", "cbrt",
-    "media", "desvio_padrao", "linearize", "compare","montecarlo"] + nomes_constantes
+    "registra_unidades", "media", "desvio_padrao", "linearize", 
+    "compare","montecarlo","quantidade_numeros_gaussianos"] + nomes_constantes + funcoes_matematicas
